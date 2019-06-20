2c43af3d96369ba7ccf6e5ec45204e36c99d1107
Koen Bijttebier <koen.bijttebier@dekimo.com>
2019-06-20 10:39:22 +0200

    allow wildcard subscriptions

--- be-mysql.c
+++ be-mysql.c
@@ -303,6 +303,135 @@ out:
 }
 
 /*
+	This code is originally located in the mosquitto library. It was copied to the auth plugin to make a specific implementation
+	The native Acl checking in mosquitto (acl_file) does not check on topics while subscribing. It only checks when somebody sends to a topic, and then all clients have their acl checked.
+	The mysql plugin (all mosquitto-auth-plug backends) check the subscription when the client subscribes to a topic.
+
+	What is the difference ? => When you make a subscription (ex cloudapp/+/state), then the topic contains wildcards. When you post a mqtt message, then the topic does not contain wildcards. 
+	Posting a topic with a wildcard is probably a security issue; and therefore not allowed.
+
+	This causes a problem for a MQTT client that wants to subscibe to the following topic: cloudapp/+/state; mosquitto-auth-plug does not allow this kind of subscriptions.
+	This function is a copy from the original mosquitto implementation; the only difference is that it allows wildcards in the subscription topic
+
+*/
+int qbus_topic_matches_sub(const char *sub, const char *topic, bool *result)
+{
+	size_t spos;
+
+
+	if(!result) return MOSQ_ERR_INVAL;
+	*result = false;
+
+	if(!sub || !topic || sub[0] == 0 || topic[0] == 0){
+		return MOSQ_ERR_INVAL;
+	}
+
+	if((sub[0] == '$' && topic[0] != '$')
+			|| (topic[0] == '$' && sub[0] != '$')){
+
+		return MOSQ_ERR_SUCCESS;
+	}
+
+	spos = 0;
+	_log(LOG_DEBUG, "Qbus specific ACL check, sub: %s, topic: %s", sub, topic);
+	while(sub[0] != 0){
+		//Original code (in mosquitto src) contained the lines below. 
+		// if(topic[0] == '+' || topic[0] == '#'){
+		// 	return MOSQ_ERR_INVAL;
+		// }
+		_log(LOG_DEBUG, "Acl Check, sub: %s, topic: %s", sub, topic);
+		if(sub[0] != topic[0] || topic[0] == 0){ /* Check for wildcard matches */
+			if(sub[0] == '+'){
+				/* Check for bad "+foo" or "a/+foo" subscription */
+				if(spos > 0 && sub[-1] != '/'){
+					return MOSQ_ERR_INVAL;
+				}
+				/* Check for bad "foo+" or "foo+/a" subscription */
+				if(sub[1] != 0 && sub[1] != '/'){
+					return MOSQ_ERR_INVAL;
+				}
+				spos++;
+				sub++;
+				while(topic[0] != 0 && topic[0] != '/'){
+					topic++;
+				}
+				if(topic[0] == 0 && sub[0] == 0){
+					*result = true;
+					return MOSQ_ERR_SUCCESS;
+				}
+			}else if(sub[0] == '#'){
+				/* Check for bad "foo#" subscription */
+				if(spos > 0 && sub[-1] != '/'){
+					return MOSQ_ERR_INVAL;
+				}
+				/* Check for # not the final character of the sub, e.g. "#foo" */
+				if(sub[1] != 0){
+					return MOSQ_ERR_INVAL;
+				}else{
+					_log(LOG_NOTICE, "Succcess!!!\n");
+					*result = true;
+					return MOSQ_ERR_SUCCESS;
+				}
+			}else{
+				/* Check for e.g. foo/bar matching foo/+/# */
+				if(topic[0] == 0
+						&& spos > 0
+						&& sub[-1] == '+'
+						&& sub[0] == '/'
+						&& sub[1] == '#')
+				{
+					*result = true;
+					return MOSQ_ERR_SUCCESS;
+				}
+
+				/* There is no match at this point, but is the sub invalid? */
+				while(sub[0] != 0){
+					if(sub[0] == '#' && sub[1] != 0){
+						return MOSQ_ERR_INVAL;
+					}
+					spos++;
+					sub++;
+				}
+
+				/* Valid input, but no match */
+				return MOSQ_ERR_SUCCESS;
+			}
+		}else{
+			/* sub[spos] == topic[tpos] */
+			if(topic[1] == 0){
+				/* Check for e.g. foo matching foo/# */
+				if(sub[1] == '/'
+						&& sub[2] == '#'
+						&& sub[3] == 0){
+					*result = true;
+					return MOSQ_ERR_SUCCESS;
+				}
+			}
+			spos++;
+			sub++;
+			topic++;
+			if(sub[0] == 0 && topic[0] == 0){
+				*result = true;
+				return MOSQ_ERR_SUCCESS;
+			}else if(topic[0] == 0 && sub[0] == '+' && sub[1] == 0){
+				if(spos > 0 && sub[-1] != '/'){
+					return MOSQ_ERR_INVAL;
+				}
+				spos++;
+				sub++;
+				*result = true;
+				return MOSQ_ERR_SUCCESS;
+			}
+		}
+	}
+	if((topic[0] != 0 || sub[0] != 0)){
+		*result = false;
+	}
+
+	return MOSQ_ERR_SUCCESS;
+}
+
+/*
  * Check ACL. username is the name of the connected user attempting to access
  * topic is the topic user is trying to access (may contain wildcards) acc is
  * desired type of access: read/write for subscriptions (READ) (1) for
@@ -366,7 +495,7 @@ int be_mysql_aclcheck(void *handle, const char *clientid, const char *username, 
 
 			t_expand(clientid, username, v, &expanded);
 			if (expanded && *expanded) {
-				mosquitto_topic_matches_sub(expanded, topic, &bf);
+				qbus_topic_matches_sub(expanded, topic, &bf);
 				if (bf) match = BACKEND_ALLOW;
 				_log(LOG_DEBUG, "  mysql: topic_matches(%s, %s) == %d",
 				     expanded, v, bf);
